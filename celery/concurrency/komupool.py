1473f21557359677821076ed1db6d7edae433fbf
komuW <komuw05@gmail.com>
2019-02-15 12:33:30 +0300

    init asynIO taskpool

--- celery/concurrency/komupool.py
+++ celery/concurrency/komupool.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+import json
+import asyncio
 
-from .base import BasePool, apply_target
+from .base import BasePool
 
 
 class KomuTaskPool(BasePool):
@@ -10,13 +12,15 @@ class KomuTaskPool(BasePool):
     komupool task pool (blocking, inline, fast).
 
     Taken from: https://github.com/celery/celery/blob/master/celery/concurrency/solo.py
+
+    run as:
+        celery worker -A cel:celobj --concurrency=1 --soft-time-limit=25 --loglevel=INFO --pool=komupool
     """
 
     body_can_be_buffer = True
 
     def __init__(self, *args, **kwargs):
         super(KomuTaskPool, self).__init__(*args, **kwargs)
-        # self.on_apply = apply_target  # or; concurrency.base.apply_target
         self.limit = 1
 
     def _get_info(self):
@@ -29,77 +33,45 @@ class KomuTaskPool(BasePool):
         }
 
     def on_apply(self, *args, **kwargs):
-        # import pdb
+        print("\n celery.concurrency.komupool.KomuTaskPool.on_apply start \n")
+        my_trace_task = args[0]  # we won't use this (for now)
 
-        # pdb.set_trace()
-        print("POOL_APP::", self.app)
-        print()
-        print("dir(self.app)::", dir(self.app))
-        print()
-        komu_apply_target(*args, **kwargs)
+        my_task_name = args[1][0]
+        task_uuid = args[1][1]
+        task_headers = args[1][2]
+        task_body = args[1][3]
+        task_content_type = args[1][4]
+        task_encoding = args[1][5]
 
-
-from billiard.einfo import ExceptionInfo
-from billiard.exceptions import WorkerLostError
-from celery.exceptions import WorkerShutdown, WorkerTerminate
-from celery.five import monotonic, reraise
-
-import asyncio
-
-from celery import _state
+        task = self.app.tasks[my_task_name]
+        print("\n celery.concurrency.komupool.KomuTaskPool.on_apply end \n")
+        komu_apply_target(task=task, task_uuid=task_uuid, task_body=task_body)
 
 
-def komu_apply_target(
-    target,
-    args=(),
-    kwargs={},
-    callback=None,
-    accept_callback=None,
-    pid=None,
-    getpid=os.getpid,
-    propagate=(),
-    monotonic=monotonic,
-    **_,
-):
+def komu_apply_target(task, task_uuid, task_body):
     """
     Apply function within pool context.
     
     Taken from: https://github.com/celery/celery/blob/master/celery/concurrency/
     """
+    print(
+        "\n celery.concurrency.komupool.komu_apply_target start. task_uuid={0} \n".format(task_uuid)
+    )
+
     try:
-        ret = komu_fast_trace_task(*args, **kwargs)
-    except propagate:
-        raise
-    except Exception:
-        raise
-
-
-def komu_fast_trace_task(
-    task,
-    uuid,
-    request,
-    body,
-    content_type,
-    content_encoding,
-    # loads=loads_message,
-    # _loc=_localized,
-    hostname=None,
-    **_,
-):
-    import json
-
-    args, kwargs, embed = json.loads(body)
-    # ############# YOTE ###########################
-    app = _state.get_current_app()
-    fun = app.tasks[task]
-
-    # fun = tasks[task]  # task if task_has_custom(task, "__call__") else task.run
-    fun(*args, **kwargs)
-    # #####################################################
-
-    print("START")
-    R = None
-    I = None
-    T = 6.400_004_826
-    Rstr = None
-    return (1, R, T) if I else (0, Rstr, T)
+        args, kwargs, embed = json.loads(task_body)
+        task(*args, **kwargs)
+        print("START")
+        R = None
+        I = None
+        T = 6.400_004_826
+        Rstr = None
+        print(
+            "\n celery.concurrency.komupool.komu_apply_target end. task_uuid={0} \n".format(
+                task_uuid
+            )
+        )
+        return (1, R, T) if I else (0, Rstr, T)
+    except Exception as e:
+        print("\n celery.concurrency.komupool.komu_apply_target end. error={0} \n".format(str(e)))
+        raise e
---
+++ examples/asyncio/run.py
@@ -0,0 +1,64 @@
+import celery
+
+
+"""
+run as:
+    python run.py # PRODUCER
+      or
+    celery worker \
+        -A run:celobj \
+        --concurrency=1 \
+        --soft-time-limit=25 \
+        --loglevel=INFO \
+        --pool=komupool # CONSUMER
+"""
+celobj = celery.Celery()
+
+
+@celobj.task(name="sync_req")
+def sync_req(url):
+    import requests
+
+    res = requests.get(url=url, timeout=90)
+    print("res: ", res)
+
+
+# @celobj.task(name="Async_req")
+# def Async_req(url):
+#     import asyncio
+#     import aiohttp
+
+#     async def async_main():
+#         async with aiohttp.ClientSession() as session:
+#             async with session.get(url) as resp:
+#                 print("resp statsus: ", resp.status)
+#                 res_text = await resp.text()
+#                 print(res_text[:50])
+
+#     asyncio.run(async_main(), debug=True)
+
+
+@celobj.task(name="Async_req")
+async def Async_req(url):
+    import asyncio
+    import aiohttp
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            print("resp statsus: ", resp.status)
+            res_text = await resp.text()
+            print(res_text[:50])
+
+
+if __name__ == "__main__":
+    # import pdb
+
+    # pdb.set_trace()
+    for _ in range(0, 10):
+        sync_req.delay(url="https://httpbin.org/delay/5")
+        print("!!! SYNC messages enqueued !!!")
+
+    # for _ in range(0, 10):
+    # Async_req.delay(url="https://httpbin.org/delay/10")
+    # print("!!! Async messages enqueued !!!")
+
